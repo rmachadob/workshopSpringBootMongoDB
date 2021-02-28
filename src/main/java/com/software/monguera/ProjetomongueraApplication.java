e4d0b9cc63777d547e893801169fa18c532e8380
rmachadob <ricardomachado2112@gmail.com>
2021-02-28 20:37:08 -0300

    primeiro commit

---
+++ src/main/java/com/software/monguera/ProjetomongueraApplication.java
@@ -0,0 +1,13 @@
+package com.software.monguera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongueraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongueraApplication.class, args);
+	}
+
+}
